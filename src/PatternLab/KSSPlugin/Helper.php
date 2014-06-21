f1b46603f6e0971a99f42279bb1ff493fe3edc59
Dave Olsen <dmolsen@gmail.com>
2014-06-21 15:38:02 -0400

    initial commit of the plug-in

---
+++ src/PatternLab/KSSPlugin/Helper.php
@@ -0,0 +1,134 @@
+<?php
+
+/*!
+ * Pattern Data KSS Plugin Helper Class
+ *
+ * Copyright (c) 2014 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ * Find KSS info and then add it to the relevant patterns in PatternData::$store
+ *
+ */
+
+namespace PatternLab\KSSPlugin;
+
+use \PatternLab\KSSPlugin\Parser;
+use \PatternLab\Config;
+use \PatternLab\Data;
+use \PatternLab\PatternData;
+use \PatternLab\PatternData\Helper as PatternDataHelper;
+use \PatternLab\PatternEngine;
+use \PatternLab\Render;
+use \PatternLab\Template\Helper as TemplateHelper;
+
+class Helper extends PatternDataHelper {
+	
+	protected $patternPaths    = array();
+	protected $descTemplate    = "";
+	protected $exampleTemplate = "";
+	
+	/**
+	* Sets-up some required attributes for the helper to properly run
+	*/
+	public function __construct($options = array()) {
+		
+		parent::__construct($options);
+		
+		$this->patternPaths    = $options["patternPaths"];
+		$this->descTemplate    = file_get_contents(__DIR__."/Views/descTemplate.mustache");
+		$this->exampleTemplate = file_get_contents(__DIR__."/Views/exampleTemplate.mustache");
+		
+	}
+	
+	/**
+	* Runs a KSS parser against the project and then checks each pattern against the results
+	* to see if any of them have been described via KSS. Modifies PatternData::$store with relevant
+	* information to be used later when outputting to the front-end.
+	*/
+	public function run() {
+		
+		// various set-up options
+		$options                 = array();
+		$options["patternPaths"] = $this->patternPaths;
+		PatternEngine::setup($options);
+		
+		// parse all of the CSS in the project
+		$kss = new Parser(Config::$options["sourceDir"]);
+		
+		foreach (PatternData::$store as $patternStoreKey => $patternStoreData) {
+			
+			if ($patternStoreData["category"] == "pattern") {
+				
+				// see if this pattern has a section in the loaded KSS
+				if ($kssSection = $kss->getSection($patternStoreKey)) {
+					
+					// update the name and desc based on the KSS
+					PatternData::$store[$patternStoreKey]["name"]       = $kssSection->getTitle();
+					PatternData::$store[$patternStoreKey]["desc"]       = $kssSection->getDescription();
+					PatternData::$store[$patternStoreKey]["descExists"] = true;
+					
+					// find the kss modifiers
+					$modifiers = $kssSection->getModifiers();
+					
+					if (!empty($modifiers)) {
+						
+						$patternModifiers = array();
+						
+						// work through each modifier
+						foreach ($modifiers as $modifier) {
+							
+							$name               = $modifier->getName();
+							$class              = $modifier->getClassName();
+							$desc               = $modifier->getDescription();
+							$code               = "";
+							$modifierCodeExists = false;
+							
+							// if it's not a pseudo class render it
+							if ($name[0] != ":") {
+								
+								$data    = Data::getPatternSpecificData($patternStoreKey);
+								$data    = array_merge($data,array("styleModifier" => $class));
+								
+								$srcPath = (isset($patternStoreData["pseudo"])) ? PatternData::$store[$patternStoreData["original"]]["pathName"] : $patternStoreData["pathName"];
+								$code    = Render::Pattern($srcPath,$data);
+								
+								$modifierCodeExists    = true;
+								
+							}
+							
+							// add pattern modifier info
+							$patternModifiers[] = array("modifierName"       => $name,
+														"modifierDesc"       => $desc,
+														"modifierCode"       => $code,
+														"modifierCodeExists" => $modifierCodeExists);
+							
+						}
+						
+						// this is silly but keeps it looking cleaner to me
+						$patternModifierData = array("patternModifiers" => $patternModifiers);
+						
+						// render the views for the plug-in
+						$descAddition    = TemplateHelper::$htmlLoader->render($this->descTemplate,$patternModifierData);
+						$exampleAddition = TemplateHelper::$htmlLoader->render($this->exampleTemplate,$patternModifierData);
+						
+						// add the views to the appropriate containers in the patterndata::$store
+						PatternData::$store[$patternStoreKey]["descAdditions"][]    = $descAddition;
+						PatternData::$store[$patternStoreKey]["exampleAdditions"][] = $exampleAddition;
+						
+						// add this info to the to be used on the code view
+						PatternData::$store[$patternStoreKey]["modifiers"] = $patternModifiers;
+						
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		unset($kss);
+		
+	}
+	
+}
+
---
+++ src/PatternLab/KSSPlugin/Listener.php
@@ -0,0 +1,42 @@
+<?php
+
+/*!
+ * Pattern Data KSS Plugin Listener Class
+ *
+ * Copyright (c) 2014 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ * Add the listener and call back to the event dispatcher. Is automatically found & invoked by the dispatcher.
+ *
+ */
+
+namespace PatternLab\KSSPlugin;
+
+use \PatternLab\PatternData\Event;
+use \PatternLab\KSSPlugin\Helper;
+
+class Listener extends \PatternLab\Listener {
+	
+	/**
+	* Add the listeners for this plug-in
+	*/
+	public function __construct() {
+		
+		$this->addListener("patternData.gatherEnd","runHelper");
+		
+	}
+	
+	/**
+	* Run the KSS Plugin Helper
+	* @param  {Object}        the event object with any properties that might need to be passed
+	*/
+	public function runHelper(Event $event) {
+		
+		$options = $event->getOptions();
+		
+		$KSSHelper = new Helper($options);
+		$KSSHelper->run();
+		
+	}
+	
+}
