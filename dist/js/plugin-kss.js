a458d536e0afdc9539cb8064aef16c4c387767ec
Dave Olsen <dmolsen@gmail.com>
2015-01-25 14:14:14 -0500

    creating real JS and mark-up for the plugin

---
+++ dist/js/plugin-kss.js
@@ -0,0 +1,21 @@
+var PluginKSS = {
+	
+	insert: function() {
+		
+		console.log("trying to insert");
+		
+		var name = "pattern-lab-plugin-kss-code-insert";
+		
+		var template         = document.getElementById(name+"-template");
+		var templateCompiled = Hogan.compile(template.innerHTML);
+		var templateRendered = templateCompiled.render(patternData.extraOutput);
+		
+		var p       = document.getElementById('sg-code-extra-output');
+		var n       = document.createElement('div');
+		n.id        = name;
+		n.innerHTML = templateRendered;
+		p.parentNode.insertBefore(n, p.nextSibling);
+		
+	}
+	
+}
